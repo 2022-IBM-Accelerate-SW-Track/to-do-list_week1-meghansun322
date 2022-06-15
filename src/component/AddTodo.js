9f097e6c840e323c14f56df7d04f024f2da0e724
Meghan Sun <meghansun322@gmail.com>
2022-06-15 08:20:51 -0400

    added data-testid

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -43,6 +43,7 @@ class AddTodo extends Component {
           variant="outlined"
           onChange={this.handleChange}
           value={this.state.content}
+          data-testid="new-item-button"
         />
 
         <Button
